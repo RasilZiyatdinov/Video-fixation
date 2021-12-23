15a1a75aa8cfd0336ddf9e1388a73841fd2b1e7d
SergeiNevolin <sergeinevolin9@gmail.com>
2021-12-23 15:19:55 +0500

    update migrations and templates

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,3 +1,6 @@
-from app import app
+from app import app, db
+from app.models import User, Post
 
-#if __name__ == '__main__':app.run()
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
